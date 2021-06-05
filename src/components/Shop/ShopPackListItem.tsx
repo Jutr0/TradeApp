b45d323115699417bd5a60a0867f8bac95255a08
Jutr0 <micwikski@gmail.com>
2021-06-05 13:12:37 +0200

    better shop: cards and packs display

--- src/components/Shop/ShopPackListItem.tsx
+++ src/components/Shop/ShopPackListItem.tsx
@@ -1,61 +1,112 @@
-import { Button, CardActions, CardMedia, Divider, Grid, GridList, GridListTile, ListItem, Paper } from "@material-ui/core";
+import {
+  Button,
+  CardActions,
+  CardMedia,
+  Divider,
+  Grid,
+  GridList,
+  GridListTile,
+  ListItem,
+  makeStyles,
+  Paper,
+} from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
 import React, { useEffect, useRef, useState } from "react";
 import { IImgModal } from "../../utils/customTypes";
 import CardFromPack from "./CardFromPack";
 
+const useStyles = makeStyles({
+  tilePack: {
+    display: "flex",
+    alignItems: "stretch",
+  },
+  tileCards:{
+    display: "flex",
+    alignItems:"center",
+    flexDirection:"column",
+    justifyContent:"space-evenly"
 
+  },
+  
+  cardRoot: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+});
 
-const ShopPackListItem = (props:IProps) => {
+const ShopPackListItem = (props: IProps) => {
+  const classes = useStyles();
 
-    const [cardsToRender,setCardsToRender] = useState<JSX.Element[]>([])
-    const {pack, cards, setOpenModal} = props;
+  const [cardsToRender, setCardsToRender] = useState<JSX.Element[]>([]);
+  const { pack, cards, setOpenModal } = props;
 
-    useEffect(()=>{
-            let tempCardsToRender = Array<JSX.Element>(0);
-            for(let i=0; i < cards.length ;i+=2){
-                tempCardsToRender.push(
-                    <GridListTile cols={2} rows={2}>
-                        <CardFromPack image={cards[i]} setOpenModal={(image:string)=>setOpenModal(image)}/>
-                        <CardFromPack image={cards[i+1]} setOpenModal={(image:string)=>setOpenModal(image)}/>
-                    </GridListTile>
-                )
-            }
+  useEffect(() => {
+    let tempCardsToRender = Array<JSX.Element>(0);
+    for (let i = 0; i < cards.length; i += 2) {
+      tempCardsToRender.push(
+        <GridListTile
+          cols={2}
+          rows={2}
+          classes={{
+            tile: classes.tileCards,
+          }}
+        >
+          <CardFromPack
+            image={cards[i]}
+            setOpenModal={(image: string) => setOpenModal(image)}
+          />
+          <CardFromPack
+            image={cards[i + 1]}
+            setOpenModal={(image: string) => setOpenModal(image)}
+          />
+        </GridListTile>
+      );
+    }
 
-            setCardsToRender(tempCardsToRender);
+    setCardsToRender(tempCardsToRender);
+  }, []);
 
-
-    },[])
-
-    return(
-        <>        
-        <br/>
-        <Divider/>
-        <br/>
-        <ListItem >
-            <GridList cellHeight="auto" cols={9} >
-                <GridListTile cols={3} rows={2} style={{borderRight:"1px solid #dddddd90"}}>
-                    <Card elevation={0}>
-                        <CardMedia component="img" image={pack}/>
-                    <CardActions disableSpacing style={{display:"flex", justifyContent:"center"}}>
-                        <Button color="secondary" variant="contained" size="large" >Buy 25$</Button>
-                    </CardActions>
-                    </Card>
-                </GridListTile>
-            {cardsToRender} 
-            </GridList>
-            
-        </ListItem>
-
-        </>
-    )
-}
+  return (
+    <>
+      <br />
+      <Divider />
+      <br />
+      <ListItem>
+        <GridList cellHeight="auto" cols={9}>
+          <GridListTile
+            cols={3}
+            rows={2}
+            style={{ borderRight: "1px solid #dddddd90" }}
+            classes={{
+              tile: classes.tilePack,
+            }}
+          >
+            <Card elevation={0} classes={{ root: classes.cardRoot }}>
+              <CardMedia component="img" image={pack} />
+              <CardActions
+                disableSpacing
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                <Button color="secondary" variant="contained" size="large">
+                  Buy 25$
+                </Button>
+              </CardActions>
+            </Card>
+          </GridListTile>
+          {cardsToRender}
+        </GridList>
+      </ListItem>
+    </>
+  );
+};
 
 export default ShopPackListItem;
 
 type IProps = {
-    pack:string,
-    cards:string[],
-    setOpenModal:(image:string)=>void
-}
+  pack: string;
+  cards: string[];
+  setOpenModal: (image: string) => void;
+};
--- src/components/Shop/ShopPacksList.tsx
+++ src/components/Shop/ShopPacksList.tsx
@@ -14,62 +14,59 @@ const ShopPacksList = () => {
   const packsData = [
     {
       pack: "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/5/57/Knights_of_the_Frozen_Throne_-_Card_pack.png",
-      cards:[
+      cards: [
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/b/bb/Thrall%2C_Deathseer%2862861%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/2/26/Eternal_Servitude%2861835%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/2/20/Blood-Queen_Lana%27thel%2861814%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/b/bb/Thrall%2C_Deathseer%2862861%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/2/26/Eternal_Servitude%2861835%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/2/20/Blood-Queen_Lana%27thel%2861814%29.png",
-      ]
+      ],
     },
     {
       pack: "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/e/e8/Forged_in_the_Barrens_-_Card_pack.png",
-      cards:[
-
+      cards: [
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/9/95/Firemancer_Flurgl%28487662%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/7/7c/Kurtrus_Ashfallen%28487634%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/b/b2/Shadow_Hunter_Vol%27jin%28463925%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/9/95/Firemancer_Flurgl%28487662%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/7/7c/Kurtrus_Ashfallen%28487634%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/b/b2/Shadow_Hunter_Vol%27jin%28463925%29.png",
-  ]
+      ],
     },
     {
       pack: "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/1/14/Legacy_-_Golden_pack.png",
-      cards:[
-
+      cards: [
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/1/1d/High_Inquisitor_Whitemane%2890686%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/9/9a/Bloodfen_Raptor%28464741%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/6/63/Harvest_Golem%28386%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/1/1d/High_Inquisitor_Whitemane%2890686%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/9/9a/Bloodfen_Raptor%28464741%29.png",
         "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/6/63/Harvest_Golem%28386%29.png",
-  ]
+      ],
     },
     {
-        pack: "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/5/57/Knights_of_the_Frozen_Throne_-_Card_pack.png",
-        cards:[
-          "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/b/bb/Thrall%2C_Deathseer%2862861%29.png",
-          "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/2/26/Eternal_Servitude%2861835%29.png",
-          "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/2/20/Blood-Queen_Lana%27thel%2861814%29.png",
-          "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/b/bb/Thrall%2C_Deathseer%2862861%29.png",
-          "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/2/26/Eternal_Servitude%2861835%29.png",
-          "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/2/20/Blood-Queen_Lana%27thel%2861814%29.png",
-        ]
-      },
-      {
-        pack: "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/e/e8/Forged_in_the_Barrens_-_Card_pack.png",
-        cards:[
-  
-          "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/9/95/Firemancer_Flurgl%28487662%29.png",
-          "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/7/7c/Kurtrus_Ashfallen%28487634%29.png",
-          "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/b/b2/Shadow_Hunter_Vol%27jin%28463925%29.png",
-          "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/9/95/Firemancer_Flurgl%28487662%29.png",
-          "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/7/7c/Kurtrus_Ashfallen%28487634%29.png",
-          "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/b/b2/Shadow_Hunter_Vol%27jin%28463925%29.png",
-    ]
-      },
+      pack: "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/5/57/Knights_of_the_Frozen_Throne_-_Card_pack.png",
+      cards: [
+        "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/b/bb/Thrall%2C_Deathseer%2862861%29.png",
+        "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/2/26/Eternal_Servitude%2861835%29.png",
+        "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/2/20/Blood-Queen_Lana%27thel%2861814%29.png",
+        "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/b/bb/Thrall%2C_Deathseer%2862861%29.png",
+        "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/2/26/Eternal_Servitude%2861835%29.png",
+        "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/2/20/Blood-Queen_Lana%27thel%2861814%29.png",
+      ],
+    },
+    {
+      pack: "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/e/e8/Forged_in_the_Barrens_-_Card_pack.png",
+      cards: [
+        "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/9/95/Firemancer_Flurgl%28487662%29.png",
+        "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/7/7c/Kurtrus_Ashfallen%28487634%29.png",
+        "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/b/b2/Shadow_Hunter_Vol%27jin%28463925%29.png",
+        "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/9/95/Firemancer_Flurgl%28487662%29.png",
+        "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/7/7c/Kurtrus_Ashfallen%28487634%29.png",
+        "https://static.wikia.nocookie.net/hearthstone_gamepedia/images/b/b2/Shadow_Hunter_Vol%27jin%28463925%29.png",
+      ],
+    },
   ];
 
   return (
